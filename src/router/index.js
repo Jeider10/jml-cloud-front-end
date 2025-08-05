ee1ea549ad49f0576ef7cd8fbc12989a11c0a89f
jmontiel <jeider.montiel@banistmo.com>
2025-08-05 15:42:04 -0500

    Version 16

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,34 @@ import LoginView from '@/views/LoginView.vue'
 import RoleSelection from '@/components/RoleSelection.vue'
 import DashboardView from '@/views/DashboardView.vue'
 
+// styles
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@/assets/styles/tailwind.css";
+
+// layouts
+
+import Admin from "@/layouts/Admin.vue";
+import AuthLayout from "@/layouts/AuthLayout.vue";
+
+// views for Admin layout
+
+import Dashboard from "@/views/admin/Dashboard.vue";
+import AdminSettings from "@/views/admin/AdminSettings.vue";
+import AdminTables from "@/views/admin/AdminTables.vue";
+import AdminMaps from "@/views/admin/AdminMaps.vue";
+
+// views for Auth layout
+
+import UserLogin from "@/views/auth/UserLogin.vue";
+import UserRegister from "@/views/auth/UserRegister.vue";
+
+// views without layouts
+
+import LandingView from "@/views/LandingView.vue";
+import ProfileView from "@/views/ProfileView.vue";
+import IndexView from "@/views/IndexView.vue";
+
 const routes = [
   {
     path: '/',
@@ -24,8 +52,59 @@ const routes = [
     path: '/dashboard',
     name: 'Dashboard',
     component: DashboardView
-  }
-]
+  },
+  {
+    path: "/admin",
+    redirect: "/admin/dashboard",
+    component: Admin,
+    children: [
+      {
+        path: "/admin/dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "/admin/settings",
+        component: AdminSettings,
+      },
+      {
+        path: "/admin/tables",
+        component: AdminTables,
+      },
+      {
+        path: "/admin/maps",
+        component: AdminMaps,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    redirect: "/auth/login",
+    component: AuthLayout,
+    children: [
+      {
+        path: "/auth/login",
+        component: UserLogin,
+      },
+      {
+        path: "/auth/register",
+        component: UserRegister,
+      },
+    ],
+  },
+  {
+    path: "/landing",
+    component: LandingView,
+  },
+  {
+    path: "/profile",
+    component: ProfileView,
+  },
+  {
+    path: "/",
+    component: IndexView,
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/" },
+];
 
 const router = createRouter({
   history: createWebHistory(),
